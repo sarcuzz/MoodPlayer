6a5aa47422fd6dacc9afda992f511fe412aa4e1a
Sarah Couzens <scouzens@mail.yu.edu>
2019-01-06 18:56:16 -0500

    updated c

--- movie.c
+++ movie.c
@@ -4,99 +4,102 @@
 #include <stdlib.h>
 #include <string.h>
 
-void getGenre(const char* genre, int genreid)
-{
-  if(genre == "action"){
-    genreid = 28;
-      }else if(genre == "adventure"){
-    genreid = 12;
-      }else if(genre == "animation"){
-    genreid = 16;
-      }else if(genre == "comedy"){
-    genreid = 35;
-      }else if(genre == "crime"){
-    genreid = 80;      
-      }else if(genre == "documentary"){
-    genreid = 99;      
-      }else if(genre == "drama"){
-    genreid = 18;      
-      }else if(genre == "family"){
-    genreid = 10751;      
-      }else if(genre == "fantasy"){
-    genreid = 14;      
-      }else if(genre == "history"){
-    genreid = 36;      
-      }else if(genre == "horror"){
-    genreid = 27;      
-      }else if(genre == "music"){
-    genreid = 10402;      
-      }else if(genre == "mystery"){
-    genreid = 9648;      
-      }else if(genre == "romance"){
-    genreid = 10749;      
-      }else if(genre == "science fiction"){
-    genreid = 878;      
-      }else if(genre == "tv movie"){
-    genreid = 10770;      
-      }else if(genre == "thriller"){
-    genreid = 53;      
-      }else if(genre == "war"){
-    genreid = 10752;      
-      }else if(genre == "western"){
-    genreid = 37;
-      }
-}
-
 int main()
 {
 
-  int actorid;
+  int *actorid;
   int genreid;
-  const char *  genre;
-  const char *  actorname;
-
+  char *actorname;
+  char *genre;
   char *buf, *p;
   char arg1[MAXLINE], arg2[MAXLINE], content[MAXLINE];
   
-    /* Extract the two arguments */
+  // Extract the two arguments 
     if ((buf = getenv("QUERY_STRING")) != NULL) {
         p = strchr(buf, '&');
         *p = '\0';
         strcpy(arg1, buf);
         strcpy(arg2, p+1);
-        actorname =(char *) arg1;
+        actorname = (char *) arg1;
         genre =  (char *) arg2;
     }
 
-  /* gets the genre */
-  getGenre(genre, genreid);
+    // gets the genre */
+    //    genreid = getGenre(genre, genreid);
+   
+    // getting genre id 
+    if(strcmp(genre,"genre=action")==0){
+    genreid = 28;
+    }else if(strcmp(genre,"genre=adventure")==0){
+    genreid = 12;
+    }else if(strcmp(genre,"genre=animation")==0){
+    genreid = 16;
+    }else if(strcmp(genre,"genre=comedy")==0){
+    genreid = 35;
+    }else if(strcmp(genre,"genre=crime")==0){
+    genreid = 80;
+  }else if(strcmp(genre,"genre=documentary")==0){
+    genreid = 99;
+      }else if(strcmp(genre,"genre=drama")==0){
+    genreid = 18;
+      }else if(strcmp(genre,"genre=family")==0){
+    genreid = 10751;
+      }else if(strcmp(genre,"genre=fantasy")==0){
+    genreid = 14;
+      }else if(strcmp(genre,"genre=history")==0){
+    genreid = 36;
+      }else if(strcmp(genre,"genre=horror")==0){
+    genreid = 27;
+      }else if(strcmp(genre,"genre=music")==0){
+    genreid = 10402;
+      }else if(strcmp(genre,"genre=mystery")==0){
+    genreid = 9648;
+      }else if(strcmp(genre,"genre=romance")==0){
+    genreid = 10749;
+      }else if(strcmp(genre,"genre=science+fiction")==0){
+    genreid = 878;
+      }else if(strcmp(genre,"genre=tv+movie")==0){
+    genreid = 10770;
+      }else if(strcmp(genre,"genre=thriller")==0){
+    genreid = 53;
+      }else if(strcmp(genre,"genre=war")==0){
+    genreid = 10752;
+      }else if(strcmp(genre,"genre=western")==0){
+    genreid = 37;
+      }
+
+
+
+        /*make a local copy of the string that we can manipulate */
+        char *const copy = strdup(actorname);
+        char *space = copy;
+        /*find the next space in the string, and replace it with a %20 */
+        while (space = strchr(space, '+')){ *space = "%%20";}
+        actorname  = copy;
+
 
 
   /* use search API request to get json that has actor id                                                                                                      
 https://api.themoviedb.org/3/search/person?api_key=5896d89b88e4261a1d2413a2846e7728&language=en\
 -US&query=firstName%20lastName&page=1&include_adult=false                                       
-/*make a local copy of the string that we can manipulate*/
-  char *const copy = strdup(actorname);
-  char *space = copy;
-  /*find the next space in the string, and replace it with a %20*/
-  while (space = strchr(space, ' ')) *space = "%%20";
-  char name = copy;
   char url[200];
-  strcpy(url,"https://api.themoviedb.org/3/search/person?api_key=5896d89b88e4261a1d2413a2846e77\
+  /*  strcpy(url,"https://api.themoviedb.org/3/search/person?api_key=5896d89b88e4261a1d2413a2846e77 \
 28&language=en-US&query=");
-  strcat(url, name);
+  strcat(url, actorname);
   strcat(url, "&page=1&include_adult=false");
   strcat(url, '\0');
 
-    
+  */
   sprintf(content, "Welcome to our Movie Application! Please choose a genre and tell us who you'd like to see in a film!");
 
   /* gets the user input */
   /*scanf("%s", actorName);
     scanf("%s", genre);*/
-    sprintf(content, "%s The actor that you are in the mood for is: %s ",content, actorname);
-    sprintf(content, "%s The genre you are in the mood for is: %s", content,genre);
-   
+  sprintf(content, "%s The actor that you are in the mood for is: %s ",content, actorname);
+  sprintf(content, "%s The genre you are in the mood for is: %d", content,genreid);
+
+  sprintf(content, "%s /n  Actorname: %s, Genre: %s, Genreid: %d", content, actorname, genre, genreid);
+  
   printf("Connection: close\r\n");
   printf("Content-length: %d\r\n", (int)strlen(content));
   printf("Content-type: text/html\r\n\r\n");
