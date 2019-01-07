4216d603ec28436382fdf2b78fb1af9ef5708009
Ariella Ciment <aciment1@mail.yu.edu>
2019-01-06 20:26:43 -0500

    json

--- jsonSearch.c
+++ jsonSearch.c
@@ -5,7 +5,10 @@
 
 /*input the api json code*/
 
-static const char *JSON_STRING  ;
+
+#define JSON_FILE_PATH "jsonSearch.json"
+#define BUFFER_SIZE 5000
+#define MAX_TOKEN_COUNT 128
 
 void readfile(char* filepath, char* fileContent)
 {
@@ -20,23 +23,91 @@ void readfile(char* filepath, char* fileContent)
     fileContent[index] = '\0';
 }
 
-/*code to parse the json*/
-static int json(const char *json, jsmntok_t *tok, const char *s) {
-	if (tok->type == JSMN_STRING && (int) strlen(s) == tok->end - tok->start &&
-			strncmp(json + tok->start, s, tok->end - tok->start) == 0) {
-		return 0;
-	}
-	return -1;
+// This is where the magic happens
+int parseJSON(char *filepath, void callback(char *, char*)){
+
+    char JSON_STRING[BUFFER_SIZE];
+
+    char value[1024];
+    char key[1024];
+
+    readfile(filepath, JSON_STRING);
+
+   int i;
+   int r;
+
+   jsmn_parser p;
+   jsmntok_t t[MAX_TOKEN_COUNT];
+
+   jsmn_init(&p);
+
+   r = jsmn_parse(&p, JSON_STRING, strlen(JSON_STRING), t, sizeof(t)/(sizeof(t[0])));
+
+   if (r < 0) {
+       printf("Failed to parse JSON: %d\n", r);
+       return 1;
+   }
+
+   /* Assume the top-level element is an object */
+   if (r < 1 || t[0].type != JSMN_OBJECT) {
+       printf("Object expected\n");
+       return 1;
+   }
+
+   
+   for (i = 1; i < r; i++){
+
+       jsmntok_t json_value = t[i+1];
+       jsmntok_t json_key = t[i];
+
+
+       int string_length = json_value.end - json_value.start;
+       int key_length = json_key.end - json_key.start;
+
+       int idx;
+
+       for (idx = 0; idx < string_length; idx++){
+           value[idx] = JSON_STRING[json_value.start + idx ];
+       }
+
+       for (idx = 0; idx < key_length; idx++){
+           key[idx] = JSON_STRING[json_key.start + idx];
+       }
+
+
+       
+       value[string_length] = '\0';
+       key[key_length] = '\0';
+
+       callback(key, value);
+
+       i++;
+   }
+
+   return 0;
 }
 
-int main() {
+
+// Only prints the key and value
+void mycallback(char *key, char* value){
+  if(strcmp(key, "id")==0)
+    printf("%s : %s\n", key, value);
+}
+
+int main()
+{
+    parseJSON(JSON_FILE_PATH, mycallback);
+    return 0;
+}
+
+/*int main() {
 	int i;
 	int j;
 	jsmn_parser p;
 	
 	jsmntok_t t[index];
 
-	/*testing to make sure it does not fail*/
+	testing to make sure it does not fail
 	
 	j = jsmn_parse(&p, JSON_STRING, strlen(JSON_STRING), t, sizeof(t)/sizeof(t[index]));
 	if (j < 0) {
@@ -44,12 +115,12 @@ int main() {
 		return 1;
 	}
 	
-	/* Assume that there is something there and if not return that */
+	 Assume that there is something there and if not return that 
 	if (j < 1 || t[0].type != JSMN_OBJECT) {
 		printf("Object expected\n");
 		return 1;
 	}
-	/* Loop over the keys to get the object */
+	 Loop over the keys to get the object 
 	for (i = 1; i < j; i++) {
 		if (json(JSON_STRING, &t[i], "id") == 0) {
 			printf("The ID is: %.*s\n", t[i+1].end-t[i+1].start,
@@ -58,3 +129,4 @@ int main() {
 		}
 	}
 }
+*/
